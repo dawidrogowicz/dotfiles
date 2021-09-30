1d2ea5fe58a4b971a8a6509b50f6a871afe4d7f9
Dawid Rogowicz <rogowiczdawid@gmail.com>
2021-09-30 23:29:56 +0200

    new install - Arch/KDE

--- .config/i3/getRandBg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BG_DIR=$1
-RANDOM=$2
-NO_IMAGES=$(ls $BG_DIR | wc -l)
-IMG_INDEX=$[ ($RANDOM % $NO_IMAGES) + 1]
-FILENAME=$(ls $BG_DIR | sed -n ${IMG_INDEX}p)
-FULL_PATH=$(realpath $BG_DIR)/$FILENAME
-feh --bg-fill $FULL_PATH
