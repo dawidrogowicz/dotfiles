802d9dd97cd47335d2fdaa4db7eaf80591c1b00b
Dawid Rogowicz <rogowiczdawid@gmail.com>
2021-11-11 17:04:31 +0100

    added cpu_temp and ram usage to statusbar

--- .config/dwm/statusbar.sh
+++ .config/dwm/statusbar.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-while xsetroot -name "`TZ='Europe/Warsaw' date +"%a %D %H:%M"`"
+
+while true
 do
-	sleep 10
+  cpu_temp=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))C
+
+  mem_total=$(free --giga | awk '{ if($1 == "Mem:") print $2 }')G
+  mem_used=$(free --mega | awk '{ if($1 == "Mem:") print $3 }')
+  if (($mem_used >= 1000))
+  then
+    mem_used=$(bc -l <<< "scale=2; $mem_used/1000")G
+  else
+    mem_used=${mem_used}M
+  fi
+
+  datetime=$(TZ='Europe/Warsaw' date +"%a %D %H:%M")
+
+  xsetroot -name "$cpu_temp | $mem_used/$mem_total | $datetime"
+	
+  sleep 2
 done
