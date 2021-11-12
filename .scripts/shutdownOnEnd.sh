fd66c966367a85456f0504a0ef28271a0866b70e
Dawid Rogowicz <rogowiczdawid@gmail.com>
2021-11-12 23:09:50 +0100

    script to shutdown on process end

---
+++ .scripts/shutdownOnEnd.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+then
+  echo "Please run as root"
+  exit 1
+fi
+
+if ! ps --pid $1
+then
+  echo "No such process running"
+  exit 1
+fi
+
+while true
+do
+  if ps --pid $1
+  then
+    echo "process still running"
+  else
+    shutdown -h now
+  fi
+
+  sleep 5
+done
