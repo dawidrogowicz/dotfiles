784b8fe97d72ff04b32e0571bdfa43e6706b4b3d
Dawid Rogowicz <rogowiczdawid@gmail.com>
2021-11-18 19:38:37 +0100

    added volume indicator to statusbar

--- .config/dwm/statusbar.sh
+++ .config/dwm/statusbar.sh
@@ -14,9 +14,17 @@ do
     mem_used=${mem_used}M
   fi
 
+  mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{ print $2 }')
+  if [ "$mute" == "yes" ]
+  then
+    volume="mute"
+  else
+    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk -F/ '{ print $2 }')
+  fi
+
   datetime=$(TZ='Europe/Warsaw' date +"%a %D %H:%M")
 
-  xsetroot -name "$cpu_temp | $mem_used/$mem_total | $datetime"
+  xsetroot -name "[$cpu_temp | $mem_used/$mem_total] [$volume] $datetime"
 	
   sleep 2
 done
