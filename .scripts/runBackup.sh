7368145fbbfba46ff0881954c547e83c7dd93911
Dawid Rogowicz <rogowiczdawid@gmail.com>
2021-12-18 09:30:30 +0100

    changed window tagging and backup script

--- .scripts/runBackup.sh
+++ .scripts/runBackup.sh
@@ -7,8 +7,8 @@ if [ -e /backup/incr/$DAY ] ; then
   mkdir /backup/incr/$DAY
 fi
 
-rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY/etc /etc /backup/full/etc
-rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY/var --exclude '/var/cache/*' /var /backup/full/var
-rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY/root /root /backup/full/root
-rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY/boot /boot /backup/full/boot
+rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY /etc /backup/full
+rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY --exclude '/var/cache/*' /var /backup/full
+rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY /root /backup/full
+rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY /boot /backup/full
 rsync -a --delete --quiet --inplace --backup --backup-dir=/backup/incr/$DAY/home --exclude={'/dawid/Videos','/dawid/.local','/dawid/.steam','/dawid/.cache','/dawid/.wine','/dawid/Games','/dawid/Downloads','/dawid/dev'} /home/dawid /backup/full/home
