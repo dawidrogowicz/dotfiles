77e1824e52d08e3d15bdb24dd0f78ba5d563dd63
Dawid Rogowicz <rogowiczdawid@gmail.com>
2023-04-17 07:10:30 +0200

    load dotenv fish function

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -11,8 +11,23 @@ alias edit_omf='nvim $OMF_CONFIG/init.fish'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles.git --work-tree=$HOME'
 alias config-status='config status -uno'
 alias tmux-dev='bass ~/.scripts/tmux-dev.sh'
+alias sudo=doas
 
 if status is-interactive
   fetch
 end
 
+function load-dotenv --description 'Load environment variables from .env file'
+  set -l envfile ".env"
+  if [ (count $argv) -gt 0 ]
+    set envfile $argv[1]
+  end
+
+  if test -e $envfile
+    for line in (cat $envfile)
+      set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
+    end
+  end
+end
+
+starship init fish | source
